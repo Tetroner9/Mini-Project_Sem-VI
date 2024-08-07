70fa85a489191c6e5f0941a89f5bc8d93b3a8612
Tetroner9 <omkadam2125@gmail.com>
2024-08-07 22:26:04 +0530

    done

---
+++ Xilinx Code/project_6.srcs/sources_1/new/cricket top model.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/04/2022 01:18:07 PM
+// Design Name: 
+// Module Name: cricket top module
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module cricket top module(
+
+    );
+endmodule
